78edf3844f2b03cd6bce42f5d528ad691591f393
kiyotakeshi-m1 <ponkan@kiyotamitsunozominoMacBook-Air.local>
2022-06-22 07:57:41 +0900

    add chunk ExecutionContext

--- BatchHelloWorldChunk/src/main/java/com/example/chunk/HelloProcessor.java
+++ BatchHelloWorldChunk/src/main/java/com/example/chunk/HelloProcessor.java
@@ -1,8 +1,12 @@
 package com.example.chunk;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +14,17 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class HelloProcessor implements ItemProcessor<String, String> {
 
+    @Value("#{JobExecutionContext['jobKey']}")
+    private String jobValue;
+
+    @Value("#{StepExecutionContext['stepKey']}")
+    private String stepValue;
+
+    @BeforeStep
+    public void beforeStep(StepExecution stepExecution){
+        log.info("jobKey={}",jobValue);
+        log.info("stepKey={}",stepValue);
+    }
     @Override
     public String process(String item) throws Exception {
         item = item + "◆";
