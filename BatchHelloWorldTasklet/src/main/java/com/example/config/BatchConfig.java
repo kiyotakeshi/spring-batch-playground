2a42c93356980c78897f1709e2cf67bb7688c496
kiyotakeshi-m1 <ponkan@kiyotamitsunozominoMacBook-Air.local>
2022-06-23 05:31:21 +0900

    add DefaultJobParametersValidator

--- BatchHelloWorldTasklet/src/main/java/com/example/config/BatchConfig.java
+++ BatchHelloWorldTasklet/src/main/java/com/example/config/BatchConfig.java
@@ -2,10 +2,13 @@ package com.example.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -26,6 +29,22 @@ public class BatchConfig {
 
     @Qualifier("HelloTasklet2")
     private final Tasklet helloTasklet2;
+
+    @Bean
+    public JobParametersValidator defaultValidator() {
+        // パラメータの必須チェックができる
+        var validator = new DefaultJobParametersValidator();
+
+        String[] requiredKeys = {"run.id", "require"};
+        validator.setRequiredKeys(requiredKeys);
+
+        String[] optionKeys = {"option"};
+        validator.setOptionalKeys(optionKeys);
+
+        validator.afterPropertiesSet();
+
+        return validator;
+    }
 
     @Bean
     public Step taskletStep1() {
@@ -49,6 +68,7 @@ public class BatchConfig {
                 .incrementer(new RunIdIncrementer())
                 .start(taskletStep1())
                 .next(taskletStep2())
+                .validator(defaultValidator())
                 .build();
     }
 }
