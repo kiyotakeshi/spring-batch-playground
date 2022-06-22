f8895015f87c1f9b098b714c59264c8f4e5c6c2a
kiyotakeshi-m1 <ponkan@kiyotamitsunozominoMacBook-Air.local>
2022-06-22 09:30:24 +0900

    add jobParameters

--- BatchHelloWorldTasklet/src/main/java/com/example/tasklet/HelloTasklet.java
+++ BatchHelloWorldTasklet/src/main/java/com/example/tasklet/HelloTasklet.java
@@ -7,12 +7,21 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("HelloTasklet")
 @StepScope // step が異なると別のインスタンスが生成される(singleton ではない)
 @Slf4j
 public class HelloTasklet implements Tasklet {
+
+    // @StepScope,JobScope の bean は jobParameters を利用できる
+    // $ java -jar BatchHelloWorldTasklet/target/BatchHelloWorldTasklet-0.0.1-SNAPSHOT.jar require="hoge" option=100
+    @Value("#{jobParameters['require']}")
+    private String require;
+
+    @Value("#{jobParameters['option']}")
+    private Integer option;
 
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
@@ -31,6 +40,9 @@ public class HelloTasklet implements Tasklet {
                 .getExecutionContext();
 
         stepContext.put("stepKey", "stepValue from HelloTasklet");
+
+        log.info("require={}",require);
+        log.info("option={}",option);
 
         return RepeatStatus.FINISHED;
     }
